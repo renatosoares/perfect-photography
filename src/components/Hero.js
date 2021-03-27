1949e81a2e9b75ca872bdf7123ff86c1904aa5ba
Renato Soares <renatosoarespro@hotmail.com>
2021-03-27 04:32:15 -0300

    init first stories

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => (
+  <div>
+    <div>
+      <h1>Ullam natus quos fugiat facere ad optio nisi neque vel incidunt</h1>
+      <ul>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+      </ul>
+      <button>Dolor sit amet</button>
+    </div>
+  </div>
+);
+
+export default Hero;

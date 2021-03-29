5e4a7bb5967bcda5bb2b48e45c0812d3dc2e6836
Renato Soares <renatosoarespro@hotmail.com>
2021-03-29 12:34:35 -0300

    add styled component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,38 @@
 import React from "react";
+import styled from "styled-components";
 
-import "./Hero.css";
+const Title = styled.h1`
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap");
+
+  font-family: "Poppins", sans-serif;
+  font-weight: 700;
+  letter-spacing: 2px;
+`;
+
+const Content = styled.div`
+  p,
+  li {
+    font-size: 20px;
+    font-weight: 300;
+  }
+
+  ul {
+    list-style: none;
+    padding-left: 0;
+  }
+
+  li {
+    &::before {
+      content: "\\2713\\0020";
+    }
+  }
+`;
 
 const Hero = ({ title, children }) => (
   <div>
     <div>
-      <h1>{title}</h1>
-      {children}
+      <Title>{title}</Title>
+      <Content>{children}</Content>
     </div>
   </div>
 );

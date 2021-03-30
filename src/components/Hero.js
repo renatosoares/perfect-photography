682411315bee11473012fd888a86cf0eb340132b
Renato Soares <renatosoarespro@hotmail.com>
2021-03-30 07:34:38 -0300

    add  media queries

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,6 +3,7 @@ import styled, { css } from "styled-components";
 
 const Root = styled.div`
   color: #fff;
+  padding: 100px 0;
 
   ${(props) => css`
     background: url(${props.image}), rgba(0, 0, 0, 0.4);
@@ -36,12 +37,26 @@ const Content = styled.div`
   }
 `;
 
+const Container = styled.div`
+  width: 100%;
+  padding: 0 8px;
+
+  @media (min-width: 1200px) {
+    padding: 0 16px;
+  }
+
+  @media (min-width: 1200px) {
+    width: 1140px;
+    margin: 0 auto;
+  }
+`;
+
 const Hero = ({ image, title, children }) => (
   <Root image={image}>
-    <div>
+    <Container>
       <Title>{title}</Title>
       <Content>{children}</Content>
-    </div>
+    </Container>
   </Root>
 );
 

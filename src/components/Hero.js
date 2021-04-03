85871d1e0959a7c10ccea50758563e08b7b466fc
Renato Soares <renatosoarespro@hotmail.com>
2021-04-03 06:05:44 -0300

    adds test to component Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -54,7 +54,7 @@ const Container = styled.div`
 `;
 
 const Hero = ({ image, children }) => (
-  <Root image={image}>
+  <Root image={image} data-testid="hero">
     <Container>
       <Content>{children}</Content>
     </Container>
---
+++ src/components/Hero.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Hero from "./Hero";
+
+test("renders Hero with children", () => {
+  const image = "https://test/image.jpg";
+  const { getByText } = render(
+    <Hero image={image}>
+      <p>Labore neque id ducimus dolore.</p>
+    </Hero>
+  );
+
+  expect(getByText("Labore neque id ducimus dolore.")).toBeInTheDocument();
+});
+
+test("renders image background", () => {
+  const image = "https://test/image.jpg";
+  const { getByTestId } = render(<Hero image={image} />);
+
+  expect(getByTestId("hero")).toHaveStyleRule({
+    backgroundImage: image,
+  });
+});

83b5f724a386c088697b467c2973d17bcb2821ca
Renato Soares <renatosoarespro@hotmail.com>
2021-04-06 08:38:03 -0300

    improves color definition

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,18 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+const getPrimaryColor = (props) => props.theme.colors.primary.main;
+const getPrimaryText = (props) => props.theme.colors.primary.text;
+
 const Button = styled.button`
   font-size: 1rem;
   font-weight: 600;
   text-transform: uppercase;
   padding: 12px 36px;
   cursor: pointer;
-  background-color: ${(props) => props.theme.colorYellow};
-  border: 2px solid ${(props) => props.theme.colorYellow};
-  color: #212121;
+  background-color: ${getPrimaryColor};
+  border: 2px solid ${getPrimaryColor};
+  color: ${getPrimaryText};
 `;
 
 Button.defaultProps = {

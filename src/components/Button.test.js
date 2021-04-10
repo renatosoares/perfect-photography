cff009a1dc49355e3dcb5eaaaf64847a3e74c32a
Renato Soares <renatosoarespro@hotmail.com>
2021-04-10 05:54:39 -0300

    increments test for click event

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render } from "test-utils";
+import { render, fireEvent } from "test-utils";
 
 import Button from "./Button";
 
@@ -8,4 +8,14 @@ test("renders a text", () => {
   const { getByText } = render(<Button>Click here</Button>);
 
   expect(getByText("Click here")).toBeInTheDocument();
+});
+
+test("trigger event on click", () => {
+  const handleClick = jest.fn();
+
+  const { getByRole } = render(<Button onClick={handleClick} />);
+
+  fireEvent.click(getByRole("button"));
+
+  expect(handleClick).toBeCalled();
 });

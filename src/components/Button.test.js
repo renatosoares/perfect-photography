0d5d3f18e9f19ae60315b002b340dd24e36c8406
Renato Soares <renatosoarespro@hotmail.com>
2021-04-10 07:05:20 -0300

    changes the test to accept the different button properties

--- src/components/Button.test.js
+++ src/components/Button.test.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { render, fireEvent } from "test-utils";
 
-import Button from "./Button";
+import Button, { ButtonColors, ButtonsVariants } from "./Button";
 
 test("renders a text", () => {
   const { getByText } = render(<Button>Click here</Button>);
@@ -18,4 +18,34 @@ test("trigger event on click", () => {
   fireEvent.click(getByRole("button"));
 
   expect(handleClick).toBeCalled();
+});
+
+test.each(Object.values(ButtonColors).map((item) => [item]))(
+  "renders with color %s",
+  (color) => {
+    const { asFragment } = render(<Button color={color} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  }
+);
+
+test.each(Object.values(ButtonsVariants).map((item) => [item]))(
+  "renders with variant %s",
+  (variant) => {
+    const { asFragment } = render(<Button variant={variant} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  }
+);
+
+test("renders with color primary and variant outlined", () => {
+  const { asFragment } = render(<Button variant="outlined" color="primary" />);
+
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test("renders with color primary and variant link", () => {
+  const { asFragment } = render(<Button variant="link" color="primary" />);
+
+  expect(asFragment()).toMatchSnapshot();
 });

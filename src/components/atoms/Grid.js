c6282612c5f6019e3c085ad6bd520371a475652f
Renato Soares <renatosoarespro@hotmail.com>
2021-04-16 10:41:55 -0300

    add test for component Grid

--- src/components/atoms/Grid.js
+++ src/components/atoms/Grid.js
@@ -1,7 +1,15 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import PropTypes from "prop-types";
 
 import { breakAt, BreakpointSizes } from "styles/Breakpoints";
+
+const breakColumnAt = (size) => (props) =>
+  props[size] &&
+  css`
+    ${breakAt(BreakpointSizes[size])} {
+      grid-template-columns: repeat(${(props) => props[size]}, 1fr);
+    }
+  `;
 
 const Grid = styled.div`
   display: grid;
@@ -9,21 +17,10 @@ const Grid = styled.div`
   grid-row-gap: 16px;
   grid-template-columns: 1fr;
 
-  ${breakAt(BreakpointSizes.sm)} {
-    grid-template-columns: repeat(${(props) => props.sm}, 1fr);
-  }
-
-  ${breakAt(BreakpointSizes.md)} {
-    grid-template-columns: repeat(${(props) => props.md}, 1fr);
-  }
-
-  ${breakAt(BreakpointSizes.lg)} {
-    grid-template-columns: repeat(${(props) => props.lg}, 1fr);
-  }
-
-  ${breakAt(BreakpointSizes.xl)} {
-    grid-template-columns: repeat(${(props) => props.xl}, 1fr);
-  }
+  ${breakColumnAt("sm")}
+  ${breakColumnAt("md")}
+  ${breakColumnAt("lg")}
+  ${breakColumnAt("xl")}
 `;
 
 Grid.defaultProps = {

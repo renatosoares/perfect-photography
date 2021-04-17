a07bdefdc0883370b9db781e247eeec608e2b7c8
Renato Soares <renatosoarespro@hotmail.com>
2021-04-17 05:37:50 -0300

    add components into App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  FaChargingStation,
+  FaCameraRetro,
+  FaImage,
+  FaUserClock,
+} from "react-icons/fa";
+
+import Hero from "components/molecules/Hero";
+import Heading from "components/atoms/Heading";
+import Button from "components/atoms/Button";
+import Grid from "components/atoms/Grid";
+import Feature from "components/atoms/Feature";
+import ThemeProvider from "styles/ThemeProvider";
+import GlobalStyle from "styles/GlobalStyle";
+
+import BirdImage from "assets/bg-bird.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider>
+      <GlobalStyle />
+      <Hero image={BirdImage}>
+        <Heading>
+          <h1>
+            Dolor sit amet <strong>consectetur</strong>
+            <br />
+            adipisicing elitit
+          </h1>
+        </Heading>
+        <ul>
+          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+        </ul>
+        <Button color="primary" variant="outlined">
+          Dolor sit amet
+        </Button>
+      </Hero>
+      <Grid sm={2} md={4}>
+        <Feature
+          icon={<FaChargingStation />}
+          title="Adipisicing elit, consectetur"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        </Feature>
+        <Feature icon={<FaCameraRetro />} title="Ipsum dolor sit consectetur">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        </Feature>
+        <Feature icon={<FaImage />} title="Ipsum dolor">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        </Feature>
+        <Feature icon={<FaUserClock />} title="Lorem ipsum dolor sit">
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        </Feature>
+      </Grid>
+    </ThemeProvider>
   );
 }
 

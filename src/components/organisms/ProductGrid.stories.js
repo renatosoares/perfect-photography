682f86b70a450f9a62ff6eaea7f0d527f1321667
Renato Soares <renatosoarespro@hotmail.com>
2021-04-22 08:14:19 -0300

    define image to component

--- src/components/organisms/ProductGrid.stories.js
+++ src/components/organisms/ProductGrid.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import ProductGrid from "./ProductGrid";
+import CupFlowerImage from "stories/assets/cup-flower.jpg";
 
 export default {
   title: "Components/Organisms/ProductGrid",
@@ -10,9 +11,12 @@ export default {
 export const usage = () => (
   <ProductGrid
     products={[
-      { id: 1, title: "Título", summary: "Texto" },
-      { id: 2, title: "Título", summary: "Texto" },
-      { id: 3, title: "Título", summary: "Texto" },
+      { id: 1, image: CupFlowerImage, title: "Título 1", summary: "Texto" },
+      { id: 2, image: CupFlowerImage, title: "Título 2", summary: "Texto" },
+      { id: 3, image: CupFlowerImage, title: "Título 3", summary: "Texto" },
+      { id: 4, image: CupFlowerImage, title: "Título 4", summary: "Texto" },
+      { id: 5, image: CupFlowerImage, title: "Título 5", summary: "Texto" },
+      { id: 6, image: CupFlowerImage, title: "Título 6", summary: "Texto" },
     ]}
   ></ProductGrid>
 );

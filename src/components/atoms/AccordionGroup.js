9180a3f27bb39a14fe28acb80a94cfb959cc1b88
Renato Soares <renatosoarespro@hotmail.com>
2021-04-25 05:19:02 -0300

    controlling state by group

--- src/components/atoms/AccordionGroup.js
+++ src/components/atoms/AccordionGroup.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const AccordionGroup = ({ children }) => children;
+const AccordionGroup = ({ children }) => {
+  const [index, setIndex] = useState(-1);
+
+  return React.Children.map(children, (child, childIndex) =>
+    React.cloneElement(child, {
+      open: childIndex === index,
+      onChange: () => {
+        if (childIndex === index) {
+          setIndex(-1);
+        } else {
+          setIndex(childIndex);
+        }
+      },
+    })
+  );
+};
 
 AccordionGroup.defaultProps = {
   children: undefined,

e4fa21d597ba64e465564421b60c50d65027ab2c
Renato Soares <renatosoarespro@hotmail.com>
2021-05-10 10:18:57 -0300

    change card componente extends with card media description

--- src/components/atoms/Card.stories.js
+++ src/components/atoms/Card.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Card, { CardBody, CardMedia } from "./Card";
+import Card, { CardBody, CardMedia, CardMediaDescription } from "./Card";
 import Section from "components/molecules/Section";
 import Heading from "components/atoms/Heading";
 import Button from "components/atoms/Button";
@@ -52,6 +52,18 @@ export const withMedia = () => (
           </Button>
         </div>
       </CardBody>
+    </Card>
+  </Section>
+);
+
+export const onlyMedia = () => (
+  <Section inverse>
+    <Card>
+      <CardMedia image={CupFlowerImage}>
+        <CardMediaDescription>
+          <h5>Media description</h5>
+        </CardMediaDescription>
+      </CardMedia>
     </Card>
   </Section>
 );

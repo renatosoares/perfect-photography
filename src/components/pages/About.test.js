b5b880dc4ac8f447fd371dd9b1d49b343a900f41
Renato Soares <renatosoarespro@hotmail.com>
2021-05-17 07:39:04 -0300

    test mock function

--- src/components/pages/About.test.js
+++ src/components/pages/About.test.js
@@ -3,7 +3,16 @@ import { render, screen } from "test-utils";
 
 import About from "./About";
 
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
 test("renders about page", () => {
   render(<About></About>);
   expect(screen.getByText("Meet our photographers")).toBeInTheDocument();
+});
+
+test("scrolls to top in first render", () => {
+  render(<About />);
+  expect(window.scrollTo).toBeCalledTimes(1);
 });
